b28800a51f6423f113745651939336f55ed84f57
SteepreE <steepreepubg@gmail.com>
2021-10-07 03:31:53 +0800

    Добавьте файлы проекта.

---
+++ ThirdLab/Coordinates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdLab
+{
+    class Coordinates
+    {
+        public double x;
+        public double y;
+        public double z;
+
+        public Coordinates(double x1, double x2, double y1, double y2, double z1, double z2)
+        {
+            this.x = x2 - x1;
+
+            this.y = y2 - y1;
+
+            this.z = z2 - z1;
+        }
+
+        public Coordinates(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Coordinates operator +(Coordinates coordinates1, Coordinates coordinates2)
+        {
+            return new Coordinates(
+                coordinates1.x + coordinates2.x,
+                coordinates1.y + coordinates2.y,
+                coordinates1.z + coordinates2.z
+                );
+        }
+
+        public static Coordinates operator -(Coordinates coordinates1, Coordinates coordinates2)
+        {
+            return new Coordinates(
+                coordinates1.x - coordinates2.x,
+                coordinates1.y - coordinates2.y,
+                coordinates1.z - coordinates2.z
+                );
+        }
+    }
+}
---
+++ ThirdLab/MyVector.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdLab
+{
+    class MyVector
+    {
+        private Coordinates _coordinates;
+
+        public MyVector(Coordinates coordinates)
+        {
+            _coordinates = coordinates;
+        }
+
+        public MyVector(double x1, double x2, double y1, double y2, double z1, double z2)
+        {
+            _coordinates = new Coordinates(x1, x2, y1, y2, z1, z2);
+        }
+
+        public MyVector(double x, double y, double z)
+        {
+            _coordinates = new Coordinates(x, y, z);
+        }
+
+        public static MyVector operator +(MyVector vec1, MyVector vec2)
+        {
+            return new MyVector(vec1._coordinates + vec2._coordinates);
+        }
+
+        public static MyVector operator -(MyVector vec1, MyVector vec2)
+        {
+            return new MyVector(vec1._coordinates - vec2._coordinates);
+        }
+
+        public static double operator *(MyVector vec1, MyVector vec2)
+        {
+            return vec1._coordinates.x * vec2._coordinates.x +
+                   vec1._coordinates.y * vec2._coordinates.y +
+                   vec1._coordinates.z * vec2._coordinates.z
+                   ;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(
+                Math.Pow(_coordinates.x, 2) +
+                Math.Pow(_coordinates.y, 2) +
+                Math.Pow(_coordinates.z, 2)
+                );
+        }
+    }
+}
+
